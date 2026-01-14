aadead37ec40764048d23602ec9370b317b55db5
Seya <31957516+sglkc@users.noreply.github.com>
2026-01-14 07:52:59 +0700

    Only launch if Xbox 360 controller is connected
    Adjusted timeout durations and added controller check for dynamic logon desktop

--- Playnite/Launcher.bat
+++ Playnite/Launcher.bat
@@ -1,6 +1,10 @@
 @ECHO OFF
 SET "PLAYNITE_PATH=%LOCALAPPDATA%\Playnite\Playnite.FullscreenApp.exe"
 
+:: Controller Early Exit Check
+powershell -NoProfile -Command ^ "$dev = Get-PnpDevice | Where-Object {$_.FriendlyName -like '*xbox 360 controller*' -and $_.Status -eq 'OK'}; if ($dev) { exit 0 } else { exit 1 }"
+IF NOT "%ERRORLEVEL%" == "0" (EXIT /B)
+
 :: Kill Explorer
 taskkill /t /f /im explorer.exe
 
@@ -22,10 +26,14 @@ START explorer.exe
 :: Wait for Playnite to start
 set "WindowTitle=Playnite"
 :waitforlauncher
-timeout /t 3
+timeout /t 1
 @for /f "Delims=:" %%a in ('tasklist /fi "WINDOWTITLE eq %WindowTitle%"') do @if %%a==INFO goto waitforlauncher
 
 :: Wait for 3 secs for Launcher fully loaded before quit/kill Boot Video
-timeout /t 3
+:: timeout /t 3
 taskkill /f /im mpv.exe
+
+:: Focus on Playnite
+START "" wscript.exe "%~dp0FocusPlaynite.vbs"
+
 exit
